c04f64fffbb562526c1327a81eab20f825a71a40
Brett Harris <gtibrett@gmail.com>
2024-05-03 12:38:12 -0400

    remove unused config on server (even tho it should be on the server)

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -9,7 +9,6 @@ export const serverSchema = z.object({
 	NODE_ENV             : z.enum(["development", "test", "production"]),
 	GITHUB_ID            : z.string(),
 	GITHUB_SECRET        : z.string(),
-	GIPHY_API_KEY        : z.string(),
 	AWS_ACCESS_KEY_ID    : z.string(),
 	AWS_SECRET_ACCESS_KEY: z.string(),
 	AWS_BUCKET           : z.string(),
