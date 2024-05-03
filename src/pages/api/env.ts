3032fe6c14cae1dd1b7c64b4146ab1ea284f89ea
Brett Harris <gtibrett@gmail.com>
2024-05-03 15:18:41 -0400

    env vars should have worked, but maybe they need to use the validated vars instead
    remove env api so secrets aren't exposed

--- src/pages/api/env.ts
+++
@@ -1,7 +0,0 @@
-import type {NextApiRequest, NextApiResponse} from 'next';
-import {clientEnv} from '../../env/schema.mjs';
-import {env} from '../../env/server.mjs';
-
-export default function handler(req: NextApiRequest, res: NextApiResponse<any>) {
-	res.status(200).json({process: process.env, clientEnv, env});
-}
